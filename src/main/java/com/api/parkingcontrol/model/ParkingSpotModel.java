9b1bcf1f99ce0beef91690ba988874dfebe990bf
VitorRod16 <vitorrodriguesbsb16@gmail.com>
2022-08-18 15:06:40 -0300

    Revert "Revert "fix caracter""
    This reverts commit c086ceb07fcde6a43154a1d0770402d1d83f346b.

---
+++ src/main/java/com/api/parkingcontrol/model/ParkingSpotModel.java
@@ -0,0 +1,51 @@
+package com.api.parkingcontrol.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TB_PARKING_SPOT")
+public class ParkingSpotModel implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true, length = 10)
+    private String number;
+
+    @Column(unique = true, length = 7)
+    private String licensePlateCar;
+
+    @Column(length = 70)
+    private String brandCar;
+
+    @Column(length = 70)
+    private String modelCar;
+
+    @Column(length = 70)
+    private String colorCar;
+
+    @Column
+    private LocalDateTime registrationDate;
+
+    @Column(length = 130)
+    private String responsibleName;
+
+    @Column(length = 30)
+    private String apartment;
+
+    @Column(length = 30)
+    private String block;
+}
